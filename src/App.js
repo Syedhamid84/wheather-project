dfb99e626cf11f3105f53318cc26efe7e424f453
CodeSprint01 <syedhamidali84@gmail.com>
2024-02-28 02:05:38 +0500

    fixes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
-import logo from './logo.svg';
 import { MainComponent } from './Maincomponent/MainComponent';
 import WeatherComponent from './Weather/Weather.component';
-import Location from './WeatherLocation/WeatherLocation';
 import { ChildComponent } from './Childcomponent/ChildComponent';
-// import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* <WeatherComponent/>
-      <MainComponent/>
-      {/* <div className='mt-4'> 
-      <Location/>
-      </div> */}
+
       <Routes>
         <Route path="/" element={<WeatherComponent />}></Route>
         <Route path="parent" element={<MainComponent />}> </Route>
